4b0f27aee90b30f40f0bb60bd945823d6538f9d7
KhawerAbbas7 <85282172+KhawerAbbas7@users.noreply.github.com>
2025-05-07 00:50:46 +0500

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'khawigifs.vercel.app'], // Add the GIF domain here
     formats: ['image/webp', 'image/avif']
   }
 }
