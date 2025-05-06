1e8074f1c2d8fe089063789d238e4ea3276e53fa
KhawerAbbas7 <85282172+KhawerAbbas7@users.noreply.github.com>
2025-05-07 00:36:37 +0500

    update components/GifCard.js

--- components/GifCard.js
+++ components/GifCard.js
@@ -7,13 +7,10 @@ export default function GifCard({ gif }) {
       <Link href={`/${encodeURIComponent(gif.name)}`}>
         <a>
           <div className="card-image">
-            <Image 
-              src={`https://khawigifs.vercel.app//gifs/${gif.path}`}
-              alt={gif.name}
-              layout="fill"
-              objectFit="cover"
-              priority
-            />
+            <img src={`https://khawigifs.vercel.app/gifs/${gif.path}`}
+      alt={gif.name}
+      style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+   />
           </div>
           <div className="card-content">
             <h3>{gif.name.replace(/\.[^/.]+$/, '')}</h3>
