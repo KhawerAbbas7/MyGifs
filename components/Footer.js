1babd665c6326d32da09ab333038c1afb75bc37b
KhawerAbbas7 <85282172+KhawerAbbas7@users.noreply.github.com>
2025-05-06 23:28:06 +0500

    update components/Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        Created by <a href="https://khawi.vercel.app" target="_blank" rel="noopener noreferrer">Khawer Abbas</a>
+      </p>
+    </footer>
+  );
+}
