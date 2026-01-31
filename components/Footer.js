2ac504c152d6a63abcdb16d9dcbecb219fcd0635
KhawerAbbas7 <85282172+KhawerAbbas7@users.noreply.github.com>
2026-01-31 14:46:54 +0500

    Delete components directory

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p>
-        Created by <a href="https://khawi.vercel.app" target="_blank" rel="noopener noreferrer">Khawer Abbas</a>
-      </p>
-    </footer>
-  );
-}
