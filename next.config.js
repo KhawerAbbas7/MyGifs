31db6c94000ed18598b21c64b83ade80b8fff881
KhawerAbbas7 <85282172+KhawerAbbas7@users.noreply.github.com>
2026-01-31 14:48:02 +0500

    Delete the file at 'next.config.js'

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['localhost', 'khawigifs.vercel.app'], // Add the GIF domain here
-    formats: ['image/webp', 'image/avif']
-  }
-}
